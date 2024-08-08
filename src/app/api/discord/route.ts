5240ac3036af0a5312ab340efd78f913a0078b8e
Zetta <zinv@coinmeca.net>
2024-08-08 17:30:18 +0900

    test - discord

--- src/app/api/discord/route.ts
+++ src/app/api/discord/route.ts
@@ -1,53 +1,28 @@
 ﻿import { InteractionType, InteractionResponseType, verifyKey } from "discord-interactions";
-import { getRandomEmoji } from "./utils";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
     const signature = req.headers.get("x-signature-ed25519");
     const timestamp = req.headers.get("x-signature-timestamp");
 
-    // Check if signature or timestamp is null
     if (!signature || !timestamp) {
         return NextResponse.json({ error: "Missing signature or timestamp" }, { status: 400 });
     }
 
     const rawBody = await req.text();
 
-    // Verify the request using the public key
     try {
         verifyKey(rawBody, signature, timestamp, process.env.PUBLIC_KEY!);
     } catch (err) {
         return NextResponse.json({ error: "Bad request signature" }, { status: 401 });
     }
 
-    // Parse the JSON body after verification
     const body = JSON.parse(rawBody);
-    const { type, data } = body;
+    const { type } = body;
 
-    // Handle verification requests
     if (type === InteractionType.PING) {
         return NextResponse.json({ type: InteractionResponseType.PONG });
     }
 
-    // Handle slash command requests
-    if (type === InteractionType.APPLICATION_COMMAND) {
-        const { name } = data;
-
-        // "test" command
-        if (name === "test") {
-            // Send a message into the channel where the command was triggered from
-            return NextResponse.json({
-                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                data: {
-                    content: `hello world ${getRandomEmoji()}`,
-                },
-            });
-        }
-
-        console.error(`unknown command: ${name}`);
-        return NextResponse.json({ error: "unknown command" }, { status: 400 });
-    }
-
-    console.error("unknown interaction type", type);
-    return NextResponse.json({ error: "unknown interaction type" }, { status: 400 });
+    // Handle other interaction types...
 }
