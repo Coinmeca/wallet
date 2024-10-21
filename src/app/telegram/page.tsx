86a18ee4f9da1eded31ffeb187dd8317cd5e090b
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2024-10-21 21:51:46 +0900

    Update - passcode

--- src/app/telegram/page.tsx
+++ src/app/telegram/page.tsx
@@ -1,4 +1,4 @@
-﻿'use client';
+﻿"use client";
 
 import { Controls, Elements, Layouts } from "@coinmeca/ui/components";
 import { Parts } from "@coinmeca/ui/index";
@@ -6,38 +6,53 @@ import { useState } from "react";
 
 export default function Lock() {
     const length = 6;
-    const [passcode, setPasscode] = useState('');
+    const [passcode, setPasscode] = useState("");
     const [error, setError] = useState(false);
     const handlePasscodeClick = (v: string) => {
         if (v?.length > length) return;
         else if (v?.length === length) {
             setError(true);
-            console.log('verify');
+            console.log("verify");
         } else {
             setError(false);
-            console.log('remove');
+            console.log("remove");
         }
         console.log(v);
         setPasscode(v);
-    }
+    };
 
-    return <Layouts.Contents.SlideContainer
-        contents={[
-            {
-                active: true,
-                children: <Layouts.Col gap={4} align={'center'} fill>
-                    <Elements.Passcode index={passcode.length} length={length} error={error} gap={'5%'} />
-                    <Elements.Text>Please type your PIN code.</Elements.Text>
-                    <Controls.Button fit>Forget your pin code?</Controls.Button>
-                </Layouts.Col>,
-            },
-            {
-                active: true,
-                children: <Layouts.Col fill>
-                    <Parts.Numberpad type="code" value={passcode} onChange={(e: any, v: any) => handlePasscodeClick(v)} style={{background:'rgba(var(--black),.45)'}} />
-                </Layouts.Col>,
-            }
-        ]}
-        vertical={true}
-    />
+    return (
+        <Layouts.Contents.SlideContainer
+            contents={[
+                {
+                    active: true,
+                    children: (
+                        <Layouts.Col gap={4} align={"center"} fill>
+                            <Layouts.Col gap={4} align={"center"} fit>
+                                <Elements.Text weight={"bold"} size={2}>
+                                    PIN
+                                </Elements.Text>
+                                <Elements.Passcode index={passcode.length} length={length} error={error} gap={"5%"} effect />
+                                <Controls.Button fit>Forget your pin code?</Controls.Button>
+                            </Layouts.Col>
+                        </Layouts.Col>
+                    ),
+                },
+                {
+                    active: true,
+                    children: (
+                        <Layouts.Col fill>
+                            <Parts.Numberpad
+                                type="code"
+                                value={passcode}
+                                onChange={(e: any, v: any) => handlePasscodeClick(v)}
+                                style={{ background: "rgba(var(--black),.45)" }}
+                            />
+                        </Layouts.Col>
+                    ),
+                },
+            ]}
+            vertical={true}
+        />
+    );
 }
