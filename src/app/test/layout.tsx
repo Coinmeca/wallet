d82e6eeaf4e139e5ca0d8423b78af52b5a44d26e
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2024-11-15 06:22:37 +0900

    test deploy

--- src/app/test/layout.tsx
+++ src/app/test/layout.tsx
@@ -1,6 +1,6 @@
 ﻿"use client";
 
-import { CoinmecaWalletAdapterContextProvider } from "@coinmeca/wallet-provider";
+import { CoinmecaWalletAdapterContextProvider } from "@coinmeca/wallet-provider/adapter";
 
 export default function Layout({ children }: any) {
     return <CoinmecaWalletAdapterContextProvider>{children}</CoinmecaWalletAdapterContextProvider>;
