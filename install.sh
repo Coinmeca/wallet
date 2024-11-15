fdf36654681a5f9ebc13e1609e76e133d5e22d30
Zetta <zinv@coinmeca.net>
2024-11-15 11:36:30 +0900

    test vercel

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 export $(cat .env | grep -v '^#' | xargs)
 
 # Replace the submodule URLs with the GitHub token in .gitmodules
-sed -i "s|https://github.com/coinmeca/wallet-provider|https://$GITHUB_TOKEN@github.com/coinmeca/wallet-provider|g" .gitmodules
-sed -i "s|https://github.com/coinmeca/wallet-sdk|https://$GITHUB_TOKEN@github.com/coinmeca/wallet-sdk|g" .gitmodules
+sed -i "s|https://github.com/coinmeca/wallet-provider|https://$GIT_TOKEN@github.com/coinmeca/wallet-provider|g" .gitmodules
+sed -i "s|https://github.com/coinmeca/wallet-sdk|https://$GIT_TOKEN@github.com/coinmeca/wallet-sdk|g" .gitmodules
 
 # Initialize and update submodules
 git submodule update --init --recursive
