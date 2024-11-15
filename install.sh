918617eab2e1effbac72fa020811718ca2692b26
Zetta <zinv@coinmeca.net>
2024-11-15 11:58:47 +0900

    test vercel

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
-#!/bin/bash
+# Ensure the GitHub token is available as an environment variable
+export GIT_TOKEN=$GIT_TOKEN
 
-# Load environment variables from .env file
-export $(cat .env | grep -v '^#' | xargs)
-
-# Replace the submodule URLs with the GitHub token in .gitmodules
-sed -i "s|https://github.com/coinmeca/wallet-provider|https://$GIT_TOKEN@github.com/coinmeca/wallet-provider|g" .gitmodules
-sed -i "s|https://github.com/coinmeca/wallet-sdk|https://$GIT_TOKEN@github.com/coinmeca/wallet-sdk|g" .gitmodules
+# Replace the placeholder in .gitmodules with the actual token
+sed -i "s|\$GIT_TOKEN|$GIT_TOKEN|g" .gitmodules
 
 # Initialize and update submodules
 git submodule update --init --recursive
