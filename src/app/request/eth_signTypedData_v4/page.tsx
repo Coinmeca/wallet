8e123d4806ec8990b326f911b56b2b918ad762f3
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2026-03-22 01:32:03 +0900

    fixing

--- src/app/request/eth_signTypedData_v4/page.tsx
+++ src/app/request/eth_signTypedData_v4/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { TypedDataPage } from "../eth_signTypedData/page";
+import { TypedDataPage } from "../eth_signTypedData/typed-data-page";
 
 export default function Page() {
     return <TypedDataPage method={"eth_signTypedData_v4"} sign={(provider, data, signer, app) => provider?.signTypedData_v4(data, signer, app)} />;
